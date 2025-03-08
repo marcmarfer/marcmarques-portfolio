2cd745f80eafc70cacf2a1b7c991a1b95b889710
marcmarfer <marcmf2004@gmail.com>
2025-03-08 07:42:51 +0100

    feature: Make personalized cursor be hidden on phone dimensions

--- src/components/CursorEffect.vue
+++ src/components/CursorEffect.vue
@@ -19,6 +19,6 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="fixed w-[30px] h-[30px] rounded-full pointer-events-none -translate-x-1/2 -translate-y-1/2 z-[9999] border-2 border-white/80 shadow-[0_0_5px_2px_rgba(255,255,255,0.3),0_0_10px_4px_rgba(255,255,255,0.1)]" id="cursor-light"></div>
+    <div class="fixed hidden sm:block w-[30px] h-[30px] rounded-full pointer-events-none -translate-x-1/2 -translate-y-1/2 z-[9999] border-2 border-white/80 shadow-[0_0_5px_2px_rgba(255,255,255,0.3),0_0_10px_4px_rgba(255,255,255,0.1)]" id="cursor-light"></div>
 </template>
 
