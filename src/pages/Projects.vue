e23d34ed34013d92c9e2427e38cfb037e6d0ea9b
marcmarfer <marcmf2004@gmail.com>
2025-03-09 02:05:07 +0100

    Test commit

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         <h1 class="text-[2rem] md:text-center md:text-[3rem] text-white font-semibold m-0 mb-8 md:mb-12 font-['Onest',_sans-serif]">Projects</h1>
+        <p class="text-white text-center">Test commit</p>
     </div>
 </template>
