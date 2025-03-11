4a89f493af6498393187eedba9a7efc6b946b829
marcmarfer <marcmf2004@gmail.com>
2025-03-11 04:19:37 +0100

    Will this color feel better?

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,7 +13,7 @@ const props = defineProps({
 
 <template>
     <div class="h-20 overflow-hidden rounded-xl shadow-lg border-l-3 border-[#FFD700] md:border-l-0 md:hover:border-l-4 transition-all duration-75">
-        <div class="w-full h-full flex items-center font-['Manrope',_sans-serif] font-semibold bg-gradient-to-r bg-[#454545] shadow-lg p-3">
+        <div class="w-full h-full flex items-center font-['Manrope',_sans-serif] font-semibold bg-gradient-to-r bg-[#373A40] shadow-lg p-3">
             <div class="h-full flex-shrink-0 mr-4">
                 <img :src="props.image" :alt="props.title" class="h-full w-auto object-contain max-w-[60px] rounded-xl">
             </div>
